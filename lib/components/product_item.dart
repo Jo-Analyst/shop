15765861932ebda375a3048874a6f018ea408655
Joelmir R. Carvalho <joalves3carvalho@gmail.com>
2023-02-03 10:51:44 -0300

    Alterando Listen para false

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -10,7 +10,10 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(
+      context,
+      // listen: true,
+    );
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
